63842dbf541bfe238065efde4c7384434cea3e33
ishanhingle <134205529+ishanhingle@users.noreply.github.com>
2024-03-06 23:58:55 +0530

    college model added

---
+++ Models/collegeModel.js
@@ -0,0 +1,30 @@
+const mongoose=require('mongoose');
+const userModel=require('./userModel');
+const collegSchema=new mongoose.Schema({
+    name:String,
+    students:[{
+        type:mongoose.Types.ObjectId,
+        ref:userModel,
+        require:true,
+    }],
+    admins:[{
+        type:mongoose.Types.ObjectId,
+        ref:userModel
+    }],
+    complaints:[{
+        title:String,
+        description:String,
+        images:[{
+            url:String,
+            filename:String,
+        }],
+        department:{
+            type:String,
+            enum:['fees','scholarship','academics','sports','library','admissions','hostel','miscleanious']
+        },
+        user:{
+            type:mongoose.Types.ObjectId,
+            ref:userModel
+        }
+    }]
+})
