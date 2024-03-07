163e3b43becc896053651b8f40959ad93058a494
ayushg198 <151770512+ayushg198@users.noreply.github.com>
2024-03-07 16:37:15 +0530

    define userModel for the project

---
+++ Models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+      userName:{
+        type:String,
+        required:true
+      },
+      email:{
+        type:String,
+        required:true,
+        unique:true
+      },
+      college:{
+        type:String,
+        required:true,
+      },
+      role:{
+        type:String,
+        enum:["student","administrator"],
+        required:true,
+      },
+      password:{
+        type:String,
+        required:true,
+        min:[8,"password must have atleast 8 characters"]
+      }
+
+})
+
+export const User=mongoose.model("User",userSchema)
